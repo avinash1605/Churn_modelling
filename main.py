388e0c06ce8106d061a772eb35c8cf1324502aba
avinash1605 <avinash1605@gmail.com>
2019-01-31 13:58:59 +0530

    secondary commit
    code updates

--- main.py
+++ main.py
@@ -2,3 +2,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('churn_modelling.csv')
+x = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
+
+# Encoding categorical data
+# Encoding the Independent Variable
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder_x = LabelEncoder()
+x[:, 1] = labelencoder_x.fit_transform(x[:, 1])
+x[:, 2] = labelencoder_x.fit_transform(x[:, 2])
+onehotencoder = OneHotEncoder(categorical_features = [1])
+x = onehotencoder.fit_transform(x).toarray()
+x = x[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
+
